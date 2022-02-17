8255a4fd6a2c604ff122e9d06e45ff8462d2c375
Ricky Saull <ricky.saull@gmail.com>
2022-02-17 17:31:07 -0500

    Add new variable to js/scripts.js
    Replaced all previous code with the "pokemonList" variable that contains several objects in the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
+let pokemonList = [{
+  name: 'Charmander',
+  height: 0.6,
+  weight: 8.5,
+  type: ['fire']
+},
+
+{
+  name: 'Charmeleon',
+  height: 1.1,
+  weight: 19,
+  type: ['fire']
+},
+
+{
+  name: 'Charizard',
+  height: 1.7,
+  weight: 90.5,
+  type: ['fire', 'flying']
+}
+];
