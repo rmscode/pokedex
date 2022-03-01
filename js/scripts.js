60ae84781b520be03821a0a97331b14a6a0f1cae
Ricky Saull <ricky.saull@gmail.com>
2022-03-01 16:38:15 -0500

    add IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,46 @@
 // List of Pokemon and their stats
-let pokemonList = [{
-  name: 'Charmander',
-  height: 0.6,
-  weight: 8.5,
-  type: ['fire']
-},
+let pokemonRepository = (function () {
+  let pokemonList = [
+  {
+    name: 'Charmander',
+    height: 0.6,
+    weight: 8.5,
+    type: ['fire']
+  },
 
-{
-  name: 'Charmeleon',
-  height: 1.1,
-  weight: 19,
-  type: ['fire']
-},
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    weight: 19,
+    type: ['fire']
+  },
 
-{
-  name: 'Charizard',
-  height: 1.7,
-  weight: 90.5,
-  type: ['fire', 'flying']
-}
+  {
+    name: 'Charizard',
+    height: 1.7,
+    weight: 90.5,
+    type: ['fire', 'flying']
+  }
 ];
 
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+function getAll() {
+  return pokemonList;
+}
+
+return {
+  add: add,
+  getAll: getAll
+};
+
+})();
+
+
 // Send list to DOM & notate large Pokemon
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height > 1.5) {
     document.write('<p>' + pokemon.name + ' [Height: ' + pokemon.height + ']' + ' [Weight: ' + pokemon.weight + ']' + ' [Type: ' + pokemon.type + ']' + " Wow, that's big!" + '</p>')
   } else
